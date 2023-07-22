ce28cd3df0225f93d2c6e1d7cfd58f7e91e1ed12
Laxmi Prasanna <laxmiprasanna59@gmail.com>
2023-07-22 19:05:23 +0530

    created jsx markup

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,26 @@ import "./App.css";
 function App() {
 	return (
 		<>
-			<h1>Hello world</h1>
+			<div className="card">
+				<h1>Todo App</h1>
+
+				<form>
+					<input placeholder="Add your new todo" />
+					<button>+</button>
+				</form>
+
+				<ul>
+					<li>Buy a new gaming laptop</li>
+					<li>Complete a previous task</li>
+					<li>Create video for Youtube</li>
+					<li>Create a new portfolio site</li>
+				</ul>
+
+				<div className="clear-tasks">
+					<p>You have 4 pending tasks</p>
+					<button>Clear All</button>
+				</div>
+			</div>
 		</>
 	);
 }
